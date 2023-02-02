dc6bc50a46860290ac8b62fb7755a4bc30affed9
Taegyeong Lee <hanarotg@naver.com>
2023-02-03 03:40:39 +0900

    feat: 랜딩화면 로그인 버튼 배치

--- app/src/main/java/com/example/soongfi_android/MainActivity.kt
+++ app/src/main/java/com/example/soongfi_android/MainActivity.kt
@@ -12,15 +12,24 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.browser.customtabs.CustomTabsIntent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.soongfi_android.ui.theme.SoongfiAndroidTheme
 import com.example.soongfi_android.ui.theme.grey
@@ -36,28 +45,47 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Column (
-                        modifier = Modifier.fillMaxWidth()
+                        // set minsize to 480dp
+                        // - https://developer.android.com/guide/topics/large-screens/support-different-screen-sizes?hl=ko
+                        modifier = Modifier
+                            .sizeIn(maxWidth = 480.dp)
+                            .padding(all = 12.dp),
                             ){
+                        // header
+                        Column(Modifier.fillMaxWidth()) {
+                            Text("숭파이")
+                        }
                         Column(modifier = Modifier
                             .fillMaxWidth()
-                            .weight(1f, true)) {
+                            .weight(1f, true),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
 
                             // Soongfi login portal button
                             Button(onClick = {
                                 openLoginPortal(this@MainActivity)
                             },
-                                shape = RoundedCornerShape(30),
-                                modifier = Modifier.fillMaxWidth()
+                                modifier = Modifier
+                                    .width(200.dp).height(200.dp)
+                                    .shadow(elevation = 10.dp)
+                                    .clip(shape = RoundedCornerShape(100.dp)),
+                                contentPadding = PaddingValues(all = 0.dp)
                             ) {
-                                Text("숭파이 로그인")
+                                // soongfi logo
+                                Image(painter = painterResource(id = R.drawable.soongfi),
+                                    contentDescription = stringResource(id = R.string.image_description_soongfi),
+                                    modifier = Modifier.size(200.dp).clip(CircleShape)
+                                )
                             }
                         }
                         Column() {
                             HelpMessage("위 와이파이 버튼을 눌러\n숭실대학교 로그인 페이지 호출 시도해보세요.")
                             // Soongfi login portal button
                             Button(onClick = { openRouterPrivatePortal(this@MainActivity) },
-                                shape = RoundedCornerShape(30),
-                                modifier = Modifier.fillMaxWidth()
+                                // DO NOT USE THIS CASE
+                                contentPadding = PaddingValues(all = 16.dp),
+                                modifier = Modifier.fillMaxWidth().shadow(elevation = 10.dp).clip(shape = RoundedCornerShape(4.dp))
                             ) {
                                 Text("로그인 페이지 호출 실패")
                             }
